aec8ab3a33dbe0b0a47936136e3c3a55226a9214
maciejkubis <maciej.k-2000@outlook.com>
2023-05-14 16:21:21 +0200

    event front/backend update

--- lib/events/widgets/single_event.dart
+++ lib/events/widgets/single_event.dart
@@ -1,16 +1,12 @@
-import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
 import 'package:events_repository/events_repository.dart';
-import 'package:posts_repository/posts_repository.dart';
 
 import 'package:zupa_na_pietrynie/content_holder/content_holder.dart';
 import 'package:zupa_na_pietrynie/app/app.dart';
 import 'package:zupa_na_pietrynie/events/events.dart';
-import 'package:zupa_na_pietrynie/home/home.dart';
-import 'package:zupa_na_pietrynie/main_screen/main_screen.dart';
 
 class SingleEvent extends StatefulWidget {
   const SingleEvent(
@@ -31,14 +27,13 @@ class _SingleEventState extends State<SingleEvent> {
   _SingleEventState(this.event, this.isAdmin);
 
   bool isAfterPublishDate() {
-    DateTime publishDateTime = DateFormat('dd-MM-yyyy')
-        .parse(event.publishDate);
+    DateTime publishDateTime =
+        DateFormat('dd-MM-yyyy').parse(event.publishDate);
     DateTime now = DateTime.now();
     now.compareTo(publishDateTime);
     if (now.compareTo(publishDateTime) < 0) {
       return false;
-    }
-    else {
+    } else {
       return true;
     }
   }
@@ -47,107 +42,134 @@ class _SingleEventState extends State<SingleEvent> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     final String userId = context.select((AppBloc bloc) => bloc.state.user.id);
-    return
-      isAfterPublishDate() ?
-      Container(
-        decoration: BoxDecoration(
-          color: AppColors.WHITE,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(color: AppColors.GREY, blurRadius: 10, spreadRadius: 1)
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-
-            if (isAdmin)
-            Row(
-              children: [
-                Spacer(),
-                SizedBox(
-                  height: 35,
-                  child:
-                  Padding(
-                  padding: EdgeInsetsDirectional.only(top: 15, end: 10),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.red),
-                    ),
-                    
-                    // the method which is called
-                    // when button is pressed
-                    onPressed: () {
-                      context.read<EventsBloc>().add(DeleteEvent(event));
-                    }, child: Text("Usuń"),
-                  )),
-                ) // Dodanie przycisku usuń/resetuj
+    return isAfterPublishDate()
+        ? Container(
+            decoration: BoxDecoration(
+              color: AppColors.WHITE,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                    color: AppColors.GREY, blurRadius: 10, spreadRadius: 1)
               ],
             ),
-            const SizedBox(height: 13),
-            Padding(
-                padding: EdgeInsetsDirectional.only(start: 15, end: 10),
-                child: Text(
-                  event.title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
-                )),
-            const SizedBox(height: 12),
-            Padding(
-                padding: EdgeInsetsDirectional.only(start: 15, end: 10),
-                child: Text(
-                  event.description,
-                  style: TextStyle(fontSize: 15),
-                )),
-            Padding(
-                padding: EdgeInsetsDirectional.only(
-                    start: 15, end: 10, top: 10, bottom: 12),
-                child: Text(
-                  'Dni wydarzenia:',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                  ),
-                )),
-            ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: event.eventDays.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                        Padding(
-                            padding: EdgeInsetsDirectional.only(
-                                start: 15, end: 10, bottom: 7),
-                            child: Text(
-                              event.eventDays[index].dayOfEvent + " :",
-                              style: TextStyle(
-                                  fontSize: 14, fontWeight: FontWeight.w600),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (isAdmin)
+                  Row(
+                    children: [
+                      Spacer(),
+                      SizedBox(
+                        height: 35,
+                        child: Padding(
+                            padding:
+                                EdgeInsetsDirectional.only(top: 15, end: 10),
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.red),
+                              ),
+
+                              // the method which is called
+                              // when button is pressed
+                              onPressed: ()
+                                  =>
+                                  showDialog<String>(
+                                context: context,
+                                builder: (BuildContext context) => AlertDialog(
+                                  title: const Text("Chcesz usunąć wydarznie?"),
+                                  content: const Text("Pamiętaj, będzie to nieodwracalna zmiana!"),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text(MainScreenStrings.CANCEL,
+                                          style: TextStyle(color: AppColors.BLACK)),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        context.read<EventsBloc>().add(DeleteEvent(event));
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text(
+                                          "Usuń",
+                                          style: TextStyle(
+                                              color: AppColors.RED)),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              child: Text("Usuń"),
                             )),
-                        ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount:
-                                event.eventDays[index].eventElements.length,
-                            itemBuilder: (BuildContext context, int i) {
-                              return Row(
-                                children: [
-                                  const SizedBox(width: 18),
-                                  // SwitchExample(event.eventDays[index].eventElements[i].participants),
-                                  SwitchParticipation(
-                                    userId: userId,
-                                    eventElement:
-                                        event.eventDays[index].eventElements[i],
-                                    key: UniqueKey(),
-                                  ),
-                                  const SizedBox(width: 5),
-                                   SizedBox(
-                                     width: width * 0.6,
-                                          child:
-                                          RichText(
+                      ) // Dodanie przycisku usuń/resetuj
+                    ],
+                  ),
+                const SizedBox(height: 13),
+                Padding(
+                    padding: EdgeInsetsDirectional.only(start: 15, end: 10),
+                    child: Text(
+                      event.title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                    )),
+                const SizedBox(height: 12),
+                Padding(
+                    padding: EdgeInsetsDirectional.only(start: 15, end: 10),
+                    child: Text(
+                      event.description,
+                      style: TextStyle(fontSize: 15),
+                    )),
+                Padding(
+                    padding: EdgeInsetsDirectional.only(
+                        start: 15, end: 10, top: 10, bottom: 12),
+                    child: Text(
+                      'Dni wydarzenia:',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )),
+                ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: event.eventDays.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                            Padding(
+                                padding: EdgeInsetsDirectional.only(
+                                    start: 15, end: 10, bottom: 7),
+                                child: Text(
+                                  event.eventDays[index].dayOfEvent + " :",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600),
+                                )),
+                            ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemCount:
+                                    event.eventDays[index].eventElements.length,
+                                itemBuilder: (BuildContext context, int i) {
+                                  return Row(
+                                    children: [
+                                      const SizedBox(width: 18),
+                                      // SwitchExample(event.eventDays[index].eventElements[i].participants),
+                                      SwitchParticipation(
+                                        userId: userId,
+                                        eventElement: event
+                                            .eventDays[index].eventElements[i],
+                                        key: UniqueKey(),
+                                      ),
+                                      const SizedBox(width: 5),
+                                      SizedBox(
+                                          width: width * 0.6,
+                                          child: RichText(
                                             text: TextSpan(
                                               // Note: Styles for TextSpans must be explicitly defined.
                                               // Child text spans will inherit styles from parent
@@ -156,35 +178,44 @@ class _SingleEventState extends State<SingleEvent> {
                                                 color: Colors.black,
                                               ),
                                               children: <TextSpan>[
-                                                TextSpan(text: event.eventDays[index].eventElements[i]
-                                                    .hour +
-                                                    " - " +
-                                                    event.eventDays[index]
-                                                        .eventElements[i].title + "\nLiczba zgłoszeń: "),
-                                                TextSpan(text:
-                                                    event
+                                                TextSpan(
+                                                    text: event
+                                                            .eventDays[index]
+                                                            .eventElements[i]
+                                                            .hour +
+                                                        " - " +
+                                                        event
+                                                            .eventDays[index]
+                                                            .eventElements[i]
+                                                            .title +
+                                                        "\nLiczba zgłoszeń: "),
+                                                TextSpan(
+                                                    text: event
                                                         .eventDays[index]
                                                         .eventElements[i]
                                                         .participants
                                                         .length
-                                                        .toString(), style: const TextStyle(fontWeight: FontWeight.bold)),
+                                                        .toString(),
+                                                    style: const TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.bold)),
                                               ],
                                             ),
                                           )),
-                                ],
-                              );
-                            }),
-                        const SizedBox(height: 10),
-                      ]));
-                }),
-            const SizedBox(height: 20),
-          ],
-        )) :
-      Container(
-        child: Center(
-          child: Text('Już niedlugo dostęne nowe wydarzenie...'),
-        ),
-      );
+                                    ],
+                                  );
+                                }),
+                            const SizedBox(height: 10),
+                          ]));
+                    }),
+                const SizedBox(height: 20),
+              ],
+            ))
+        : Container(
+            child: Center(
+              child: Text('Już niedługo dostęne nowe wydarzenie ...'),
+            ),
+          );
   }
 }
 
