9c50d749a33e79680308313e0a1f7fa738b878b8
mkubis00 <maciej.k-2000@outlook.com>
2023-06-09 12:31:25 +0200

    elo

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
-  textTheme: GoogleFonts.openSansTextTheme(),
   primaryColorDark: const Color(0xFF0097A7),
   primaryColorLight: const Color(0xFFB2EBF2),
   primaryColor: const Color(0xFF00BCD4),
